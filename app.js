3ec0044ec9c7e712fb2d1a5bff687c453f0f8ebb
fumiya <fu38.terada@gmail.com>
2019-07-23 15:36:19 +0900

    構文とif文の組み合わせを追加
    3と5の倍数の処理を分岐（世界の車窓から）しました。@テレビ朝日

--- app.js
+++ app.js
@@ -15,10 +15,24 @@ if (score >= 80) {
   console.log(`${score}点、また来年`);
 }
 
+// ifとforの練習
 for(let i = 1; i <=30; i++){
   if(i % 3 ==0){
     console.log('アホ');
   }else{
     console.log(i);
+  }
+}
+
+// 5の倍数と3の倍数の時にメッセージ
+// console.logで出力
+// それ以外は数字
+// 1　〜　100まで
+
+for(let num = 1; num <=100; num++){
+  if(num % 3 ==0 || num % 5 ==0){
+    console.log('世界の車窓から');
+  }else{
+    console.log(num);
   }
 }
